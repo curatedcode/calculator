93671ead43b09bda71354687da7d3c104baa7a8f
zack <zackaryfotheringham@hotmail.com>
2022-06-22 06:53:59 -0400

    calc present on page

---
+++ script.js
@@ -0,0 +1,28 @@
+let numOne = 3;
+let numTwo = 4;
+
+function add(){
+    let result = numOne+numTwo;
+    console.log(result);
+};
+
+function subtract(){
+    let result = numOne-numTwo;
+    console.log(result);
+};
+
+function multiply(){
+    let result = numOne*numTwo;
+    console.log(result);
+};
+
+function divide(){
+    let result = numOne/numTwo;
+    console.log(result);
+};
+
+
+add();
+subtract();
+multiply();
+divide();
